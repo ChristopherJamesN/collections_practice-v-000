d2370d5cc8a1c6abee2ff276dbcdf87342641ce4
Chris Nady <nady.christopher@gmail.com>
2017-09-07 17:25:20 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -40,6 +40,6 @@ def sum_array (array)
   array.inject { |sum,n| sum +n}
 end
 
-def add_s (array) 
+def add_s (array)
   array.each_with_index.collect{|element, index| element = element + "s" unless index == 1 (element=element)}
 end
