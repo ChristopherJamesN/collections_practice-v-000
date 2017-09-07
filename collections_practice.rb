e3c75088de0e5cd5ee5b7e1127c1679f712309eb
Chris Nady <nady.christopher@gmail.com>
2017-09-07 17:10:39 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -22,3 +22,13 @@ end
 def reverse_array (array)
   array.reverse
 end
+
+def kesha_maker (array)
+  new_array = []
+  array.each do |name|
+    name(2)=$
+    new_array.push(name)
+  end
+  new_array
+end
+    
